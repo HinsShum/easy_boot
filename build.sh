7be1a8440dac2231299d4ef7d44b9b6490c3c4da
HinsShum <hinsshum@qq.com>
2020-11-28 22:30:05 +0800

    <Modify>: 修改build.sh，增加烧录和擦除功能

--- build.sh
+++ build.sh
@@ -11,16 +11,38 @@ function compile()
     cmake --build build
 }
 
-function build_compile()
-{
-    build
-    compile
-}
-
 function clear_build()
 {
     echo "clear build"
     rm build -rf
+}
+
+function generate_flash_jlink()
+{
+    os=`uname -s`
+    if [ $( expr $os : 'Linux' ) -gt 0 ]; then
+        jlink=JLinkExe
+    else
+        jlink=JLink
+    fi
+    rm build/download.* -f
+    rm build/erase.* -f
+    echo -e "si SWD\nspeed 4000\nr\nh\nloadbin build/EasyBoot.bin,${load_start_address}\nr\nexit" > build/download.jlink
+    echo "${jlink} -Device ${device} -If SWD -Speed 4000 -JTAGConf -1,-1 -autoconnect 1 -CommanderScript build/download.jlink" > build/download.sh
+    echo -e "si SWD\nspeed 4000\nr\nh\nerase ${load_start_address} ${load_end_address}\nexit" > build/erase.jlink
+    echo "${jlink} -Device ${device} -If SWD -Speed 4000 -JTAGConf -1,-1 -autoconnect 1 -CommanderScript build/erase.jlink" > build/erase.sh
+}
+
+function download_bin()
+{
+    cmd=`cat build/download.sh`
+    echo $cmd | sh
+}
+
+function erase_bin()
+{
+    cmd=`cat build/erase.sh`
+    echo $cmd | sh
 }
 
 function help_info()
@@ -33,6 +55,8 @@ function help_info()
     echo "  -[B|b] --name=<board name> --[debug|release]    = build the project by board name"
     echo "  -[C|c]                                          = compile the project"
     echo "  -[R|r]                                          = clear the target and the project directory"
+    echo "  --download                                      = download bin file to the device"
+    echo "  --erase                                         = erase flash on the device"
 }
 
 if [ $# -eq 0 ]; then
@@ -44,6 +68,10 @@ build=0
 compile=0
 clear=0
 build_type=Debug
+download=0
+erase=0
+support_board=("CY000001_0x08000000_0x08005000_STM32F103ZE")
+find_board=0
 
 while [ $# -gt 0 ]; do
     if [ $( expr $1 : '-[B|b]' ) -gt 0 ]; then
@@ -58,15 +86,38 @@ while [ $# -gt 0 ]; do
         build_type=Release
     elif [ $( expr $1 : '--debug' ) -gt 0 ]; then
         build_type=Debug
+    elif [ $( expr $1 : '--download' ) -gt 0 ]; then
+        download=1
+    elif [ $( expr $1 : '--erase' ) -gt 0 ]; then
+        erase=1
     elif [ $( expr $1 : '-[-help|h]' ) -gt 0 ]; then
         help_info
     fi
     shift
 done
 
+if [ -n "$board_name" ]; then
+    for loop in ${support_board[*]}
+    do
+        # split by '_'
+        support=(${loop//_/ })
+        if [ $( expr $board_name : ${support[0]} ) -gt 0 ]; then
+            find_board=1
+            load_start_address=${support[1]}
+            load_end_address=${support[2]}
+            device=${support[3]}
+        fi
+    done
+    if [ $find_board -eq 0 ]; then
+        echo "Please check the board name that you input, can not find the board name:${board_name}"
+        exit
+    fi
+fi
+
 if [ $build -gt 0 ]; then
     if [ -n "$board_name" ]; then
         build
+        generate_flash_jlink
     else
         echo "Please input the board name that you want to compile"
         exit 0
@@ -75,6 +126,14 @@ fi
 
 if [ $compile -gt 0 ]; then
     compile
+fi
+
+if [ $download -gt 0 ]; then
+    download_bin
+fi
+
+if [ $erase -gt 0 ]; then
+    erase_bin
 fi
 
 if [ $clear -gt 0 ]; then
